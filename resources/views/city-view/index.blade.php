7e8a1ed8c0f2ec2ed633f1c994e471868f22d4d8
Amlan Alok <amlanalok@gmail.com>
2021-04-15 10:40:29 -0500

    added include directive

--- resources/views/city-view/index.blade.php
+++ resources/views/city-view/index.blade.php
@@ -5,10 +5,23 @@
 @section('content')
     <h1>City View</h1>
 
-    @foreach ($roleList as $role)
+    {{-- @foreach ($roleList as $key => $role)
+    <p>{{ $key }}</p>
     <p>{{ $role['id'] ?? '' }}</p>
-    <p>{{ $role['role_name'] ?? ''}}</p>    
-    @endforeach
+    <p>{{ $role['role_name'] ?? ''}}</p>   
+    <p>----------------</p> 
+    @endforeach --}}
+
+    @forelse ($roleList as $key => $role)
+
+    {{-- @break($key == 1)
+    @continue($key == 1) --}}
+
+        @include('city-view.test')
+
+    @empty
+    <p>This is blank</p>
+    @endforelse
 
     {{-- This is how if and else is used in blade --}}
     {{-- @if($role['id'] == 2)                   
@@ -21,7 +34,7 @@
 
     {{-- This is how unless is used -> if the condition is false, then the code is rendered --}}
 
-    @unless ($role['id'] == 2)
+    {{-- @unless ($role['id'] == 2)
     <p>Unless works if the condition is false</p>
         
     @endunless
@@ -29,6 +42,6 @@
     @isset($role['id'])
     <p>id variable isset</p>
         
-    @endisset
+    @endisset --}}
 
 @endsection
