3abfe6c903391fbe76869aaad89a9b5b92a69eb0
Amlan Alok <amlanalok@gmail.com>
2021-04-15 01:01:23 -0500

    added unless directive

--- resources/views/city-view/index.blade.php
+++ resources/views/city-view/index.blade.php
@@ -5,12 +5,20 @@
 @section('content')
     <h1>City View</h1>
 
-    @if($role['id'] == 2)
+    {{-- This is how if and else is used in blade --}}
+    @if($role['id'] == 2)                   
     <p>{{ $role['id'] ?? '' }}</p>
     <p>{{ $role['role_name'] ?? ''}}</p>
     @else
     <p>This is the ELSE block</p>
 
     @endif
+
+    {{-- This is how unless is used -> if the condition is false, then the code is rendered --}}
+
+    @unless ($role['id'] == 2)
+    <p>Unless works if the condition is false</p>
+        
+    @endunless
 
 @endsection
