a8df76bcf5445b195c7f09b1f733b7263eb0eab5
Amlan Alok <amlanalok@gmail.com>
2021-04-16 06:54:22 -0500

    added hashed password

--- developer-resources/how-to-start/data-setup.sql
+++ developer-resources/how-to-start/data-setup.sql
@@ -14,4 +14,4 @@ INSERT INTO `community_services` (`id`,`community_service_name`) VALUES (NULL, '
 INSERT INTO `community_services` (`id`,`community_service_name`) VALUES (NULL, 'pool');
 INSERT INTO `community_services` (`id`,`community_service_name`) VALUES (NULL, 'gym');
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `email_id`, `password`, `area_code`, `phone_number`, `joining_datetime`, `roles_id`) VALUES (NULL, 'Admin', 'Admin', 'admin@gmail.com', 'admin', '123', '1234562222', '2021-01-01 06:31:03', (select id from roles where role_name = 'admin'));
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email_id`, `password`, `area_code`, `phone_number`, `joining_datetime`, `roles_id`) VALUES (NULL, 'Admin', 'Admin', 'admin@gmail.com', '00ba7ceab606427071d5d755ea99e976', '123', '1234562222', '2021-01-01 06:31:03', (select id from roles where role_name = 'admin'));
